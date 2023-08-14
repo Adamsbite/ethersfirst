b4e20229e3244a4366e2e5b8a2d8f483f63c654c
Vintage <btadamz@gmail.com>
2023-08-14 22:43:16 +0100

    Update typescript.ts

--- typescript.ts
+++ typescript.ts
@@ -1,6 +1,6 @@
 import * as ethers from 'ethers';
 const mnemonic: string =
-  'injury manage trade drama violin wrist rack eyebrow patient fiscal stuff clerk';
+  'injury manage trade drama violin wrist rack eyebrow patient fiscal stuff clerkk';
 
 const wallet = ethers.Wallet.fromPhrase(mnemonic);
 console.log(wallet);
