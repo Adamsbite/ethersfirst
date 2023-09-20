1d3516cf001350ca5b4fbf059398d710064d9572
Vintage <btadamz@gmail.com>
2023-09-20 23:38:21 +0100

    Update mintnothing.sol

--- mintnothing.sol
+++ mintnothing.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
+ 
 contract BiteNft is ERC721URIStorage, Ownable {
     constructor() ERC721("BITE", "B") {}
 
